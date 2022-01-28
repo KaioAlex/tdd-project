5c9234f529aefa30dfc74cc825e86b9f03f8f8b2
Caio Campos <caio.campos@dcc.ufmg.br>
2022-01-28 17:38:43 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,14 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+    def test_displays_all_items(self):
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
 class ListAndItemModelsTest(TestCase):
 
